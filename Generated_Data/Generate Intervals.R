8598e612b0dadbcb2ce395db4aadd9667fa47514
William Msemburi <excessc19@gmail.com>
2022-09-02 14:10:46 +0200

    Updating data for estimates
    Updates to the data for the estimates as well as additional code for some of the paper summaries.

--- Generated_Data/Generate Intervals.R
+++ Generated_Data/Generate Intervals.R
@@ -9,7 +9,7 @@
 rm(list = ls(all = TRUE))
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-pacman::p_load(data.table, lubridate, tidyr, dplyr, INLA)
+pacman::p_load(data.table, lubridate, tidyr, dplyr, INLA, xlsx)
 
 load("mf.df.Rda")
 
@@ -35,7 +35,8 @@ get.intervals <- function(model.lab){
   
 exp.samps        <- fread("expected.csv") %>% mk.mat()
 ac.samps         <- fread("acm.csv") %>% mk.mat()
-excess.samps     <- fread("excess.csv") %>% mk.mat()
+excess.samps     <- ac.samps - exp.samps
+pscore.samps     <- 100*excess.samps/exp.samps
 
 rep.covid    <- mf.df %>% select(covid_deaths) %>% as.matrix()
 observed.df  <- mf.df %>% select(observed) %>% as.matrix()
@@ -54,11 +55,12 @@ print("Distributions of monthly mortality predictions")
   country.ac            <- t(apply(ac.samps, 1, get.dist)) # Predicted all cause deaths
   country.exp           <- t(apply(exp.samps, 1, get.dist)) # Expected all cause deaths
   country.exc           <- t(apply(excess.samps, 1, get.dist)) # Excess deaths
+  country.pscore        <- t(apply(pscore.samps, 1, get.dist)) # pscore
   country.covid         <- rep.covid[,1]
   
   # 2 Get distribution for global 
   print(".................................By Global")
-  global.exc <- global.exp <- global.ac  <- array(dim = c(mx.m, 3)) # empty matrix to populate with distribution
+  global.pscore <- global.exc <- global.exp <- global.ac  <- array(dim = c(mx.m, 3)) # empty matrix to populate with distribution
   global.covid          <- rep(NA, mx.m)
   for (m in 1:mx.m){
     # get row indices for month m for entire matrix of posterior draws 
@@ -67,12 +69,14 @@ print("Distributions of monthly mortality predictions")
     global.ac[m,]      <- get.dist(apply(ac.samps[rows,], 2, sum, na.rm = T)) 
     global.exp[m,]     <- get.dist(apply(exp.samps[rows,], 2, sum, na.rm = T)) 
     global.exc[m,]     <- get.dist(apply(excess.samps[rows,], 2, sum, na.rm = T)) 
+    global.pscore[m,]  <- get.dist(100*apply(excess.samps[rows,], 2, sum, na.rm = T)/
+                                     apply(exp.samps[rows,], 2, sum, na.rm = T)) 
     global.covid[m]    <- sum(rep.covid[rows,1]) 
   }
   
   # 3) by region
   print(".................................By Region")
-  region.exc <- region.exp <- region.ac <- array(dim = c(mx.m*6, 3))
+  region.pscore <- region.exc <- region.exp <- region.ac <- array(dim = c(mx.m*6, 3))
   region.covid         <- rep(NA, mx.m*6)
   r = 1
   for (reg in c("AFRO", "AMRO", "EMRO", "EURO", "SEARO", "WPRO")){
@@ -83,6 +87,8 @@ print("Distributions of monthly mortality predictions")
       region.ac[r,]      <- get.dist(apply(ac.samps[rows,], 2, sum, na.rm = T))
       region.exp[r,]     <- get.dist(apply(exp.samps[rows,], 2, sum, na.rm = T))
       region.exc[r,]     <- get.dist(apply(excess.samps[rows,], 2, sum, na.rm = T))
+      region.pscore[r,]  <- get.dist(100*apply(excess.samps[rows,], 2, sum, na.rm = T)/
+                                       apply(exp.samps[rows,], 2, sum, na.rm = T))
       region.covid[r]    <- sum(rep.covid[rows,1]) 
       r = r + 1
     }
@@ -90,7 +96,7 @@ print("Distributions of monthly mortality predictions")
 
   # 4) by income group
   print(".................................By Income Group")
-  income.exc <- income.exp <- income.ac  <- array(dim = c(mx.m*4, 3))
+  income.pscore <- income.exc <- income.exp <- income.ac  <- array(dim = c(mx.m*4, 3))
   income.covid         <- rep(NA, mx.m*4)
   r = 1
   for (reg in c("LIC", "LMIC", "UMIC", "HIC")){
@@ -101,6 +107,8 @@ print("Distributions of monthly mortality predictions")
       income.ac[r,]      <- get.dist(apply(ac.samps[rows,], 2, sum, na.rm = T))
       income.exp[r,]     <- get.dist(apply(exp.samps[rows,], 2, sum, na.rm = T))
       income.exc[r,]     <- get.dist(apply(excess.samps[rows,], 2, sum, na.rm = T))
+      income.pscore[r,]     <- get.dist(100*apply(excess.samps[rows,], 2, sum, na.rm = T)/
+                                       apply(exp.samps[rows,], 2, sum, na.rm = T))
       income.covid[r]    <- sum(rep.covid[rows,1]) 
       r = r + 1
     }
@@ -112,12 +120,14 @@ print("Distributions of monthly mortality predictions")
   colnames(income.ac)<-colnames(region.ac)<-colnames(global.ac)<-colnames(country.ac)<- c("ac.fit", "ac.lwr", "ac.uppr")
   colnames(income.exp)<-colnames(region.exp)<-colnames(global.exp)<-colnames(country.exp)<- c("exp.fit", "exp.lwr", "exp.uppr")
   colnames(income.exc)<-colnames(region.exc)<-colnames(global.exc)<-colnames(country.exc)<-  c("fit", "lwr", "uppr")
-
+  colnames(income.pscore)<-colnames(region.pscore)<-colnames(global.pscore)<-colnames(country.pscore)<-  c("psc.fit", "psc.lwr", "psc.uppr")
+  
   ####################################################################################
   all.covid    <- c(country.covid, global.covid, region.covid, income.covid)
   all.expected <- rbind(country.exp, global.exp, region.exp, income.exp)  
   all.allcause <- rbind(country.ac, global.ac, region.ac, income.ac)
   all.excess   <- rbind(country.exc, global.exc, region.exc, income.exc)  
+  all.pscore   <- rbind(country.pscore, global.pscore, region.pscore, income.pscore)  
   
   ####################################################################################
   
@@ -294,6 +304,7 @@ output.table <- data.table(full.labs,
              all.allcause,
              all.excess,
              all.excess.c,
+             all.pscore, 
              c.covid21    = all.covid.c21,
              all.excess.c21,
              model.name    = model.lab) %>% 
@@ -305,5 +316,63 @@ output.table <- data.table(full.labs,
 return(output.table)
 }
 
-who.mod.ests <- get.intervals("Model All")
+loc.labs <- mf.df %>% select(Country, iso3) %>% unique() %>%
+  rename(location = iso3) %>%
+  rbind(data.table(Country = c("Global", "AFRO", "AMRO", "EMRO", "EURO", "SEARO", "WPRO", 
+                               "LIC", "LMIC", "UMIC", "HIC"),
+                   location = c("Global", "AFRO", "AMRO", "EMRO", "EURO", "SEARO", "WPRO",
+                                "LIC", "LMIC", "UMIC", "HIC")))
 
+who.mod.ests <- get.intervals("Model All") %>% left_join(loc.labs, by = "location")
+
+##################################################################################################
+# 
+
+write.xlsx(who.mod.ests,row.names = FALSE, 
+           file="Estimate.Database.xlsx",
+           sheetName="Estimates", append=FALSE)
+
+# Compare models with IHME and economist
+
+ihme2411 <- fread("https://www.dropbox.com/s/v9twekf68eecf8s/ihme1811.csv?dl=1") %>%
+  filter(! WHO_region %in% c("China", "India"))
+
+econ2411 <- rbind(fread("https://raw.github.com/TheEconomist/covid-19-the-economist-global-excess-deaths-model/main/output-data/output-by-alternative-regions/export_regions_WHO_region_cumulative.csv")  %>% 
+                    select(region, date, cumulative_estimated_daily_excess_deaths,
+                           cumulative_estimated_daily_excess_deaths_ci_95_bot,
+                           cumulative_estimated_daily_excess_deaths_ci_95_top),
+                  fread("https://raw.github.com/TheEconomist/covid-19-the-economist-global-excess-deaths-model/main/output-data/export_world_cumulative.csv") %>% rename(region = world) %>%
+                    select(region, date, cumulative_estimated_daily_excess_deaths,
+                           cumulative_estimated_daily_excess_deaths_ci_95_bot,
+                           cumulative_estimated_daily_excess_deaths_ci_95_top))  %>%
+  rename(Country = region, 
+         c.fit  = cumulative_estimated_daily_excess_deaths, 
+         c.lwr  = cumulative_estimated_daily_excess_deaths_ci_95_bot,
+         c.uppr = cumulative_estimated_daily_excess_deaths_ci_95_top) %>%
+  mutate(date  = as.Date(date), 
+         year   =  lubridate::year(lubridate::ymd(date)),
+         month  =  lubridate::month(lubridate::ymd(date)), 
+         months = ifelse(year == 2020, month, month + 12), 
+         Country = ifelse(Country == "World", "Global", Country)) %>%
+  filter(Country %in% ihme2411$WHO_region & year < 2022) %>%
+  group_by(Country, year) %>% filter(date == max(date)) %>% ungroup() %>% 
+  select(Country, months, c.fit, c.lwr, c.uppr) %>% 
+  rename(WHO_region = Country, period = months, mean = c.fit, lwr = c.lwr, uppr = c.uppr) %>% 
+  mutate(source = "Economist")
+
+
+who2411 <- who.mod.ests %>%
+  filter(model.name == "Model All" &
+           use.observed  == "yes") %>% 
+  filter(Country %in% ihme2411$WHO_region & months %in% c(12,24)) %>% 
+  select(Country, months, c.fit, c.lwr, c.uppr) %>% 
+  rename(WHO_region = Country, period = months, mean = c.fit, lwr = c.lwr, uppr = c.uppr) %>% 
+  mutate(source = "WHO")
+
+reg.ests <- rbind(ihme2411, econ2411, who2411) %>% 
+  mutate(WHO_region = factor(WHO_region, levels = c("Global", "AFRO", "AMRO", "EMRO", "EURO", "SEARO", "WPRO"))) %>%
+  arrange(period, WHO_region, source) 
+
+write.xlsx(reg.ests,row.names = FALSE, 
+           file="Model.Comparison.xlsx",
+           sheetName="Estimates", append=FALSE)
